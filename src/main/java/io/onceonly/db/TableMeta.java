9d0168504853a0b60297aab6c8dff36747ec990c
maxian <Ma@danglaoshi.net>
2017-10-19 18:48:54 +0800

    修改模型

--- src/main/java/io/onceonly/db/TableMeta.java
+++ src/main/java/io/onceonly/db/TableMeta.java
@@ -1,28 +1,67 @@
 package io.onceonly.db;
 
-import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 public class TableMeta {
-	private Class<?> entity;
-	private List<String> index;
-	private Map<String,ColumnMeta> columnMeta;
-	
+	String table;
+	String primaryKey;
+	List<String[]> uniqueConstraint;
+	Map<String,ColumnMeta> columnMeta;
+	/** drop table if exists tbl_a;*/
 	public String createTableSql() {
-		//TODO
-		return null;
+		StringBuffer tbl = new StringBuffer();
+		tbl.append(String.format("CREATE TABLE %s (", table));
+		StringBuffer FKs =  new StringBuffer();
+		for(ColumnMeta cm:columnMeta.values()) {
+			tbl.append(String.format("%s %s%s%s,", cm.name,cm.type, cm.nullable?"":" not null",cm.unique?" unique":""));
+			if(cm.useFK && cm.refTable !=null){
+				FKs.append(String.format("FOREIGN KEY (%s) REFERENCES %s(%s),",cm.name,cm.refTable,cm.name));
+			}
+		}
+		for(String[] tuple:uniqueConstraint) {
+			tbl.append(String.format("CONSTRAINT %s_%s UNIQUE (%s)",table,String.join("_", tuple),String.join(",", tuple)));
+		}
+		if(primaryKey != null) {
+			tbl.append("PRIMARY Key id,");	
+		}
+		tbl.append(FKs.toString());
+		tbl.delete(tbl.length()-1, tbl.length());
+		tbl.append(")");
+		return tbl.toString();
+	}
+
+	/**
+	 * TODO
+	 * 升级数据库，返回需要执行的sql
+	 * @param other
+	 * @return
+	 */
+	public String upgradeTo(TableMeta other) {
+		StringBuffer sql = new StringBuffer();
+		return sql.toString();
+	}
+	/**
+	 * TODO
+	 * 升级数据库，废弃的数据库字段不需要使用了，删除操作sql
+	 * @param other
+	 * @return
+	 */
+	public String cleanByTo(TableMeta other) {
+		StringBuffer sql = new StringBuffer();
+		return sql.toString();
 	}
 }
 
 class ColumnMeta {
 	String name;
 	String type;
-	String alias;
 	boolean nullable;
+	boolean unique;
+	String refTable;
+	boolean useFK;
 	/**
 	 * 正则表达式 或@JSON,@Email等特定教研
 	 */
 	String pattern;
-	Field field;
 }
--- src/test/java/io/onceonly/db/DDEngineTest.java
+++ src/test/java/io/onceonly/db/DDEngineTest.java
@@ -4,6 +4,32 @@ import org.junit.Test;
 
 public class DDEngineTest {
 
+	/**
+	User {id,name}
+	UserAddr{id,addr}
+	UserFriend {id,userId,friendId}
+	Goods {id,name}
+	GoodsDesc {id,saled,content}
+	GoodsOrder {id,goodsId,userId,amount,money,ctime}
+	GoodsShipping {id,userId,goodsOrderId,receiverId,addr }
+	1.哪些用户在情人节给他的好友购买销量小于1000，价值大于1000元戒指？
+	"select u.id userId,u.name username,ua.addr addr, g.name goodsName,go.ctime ctime
+	from GoodsOrder go
+	left join User u on go.userId = u.id
+	left join UserAddr ua on go.userId = ua.id
+	left join Goods g on g.id = go.goodsId
+	left join GoodsDesc gd on go.goodsId = gd.id
+	left join GoodsShipping gs on go.goodsOrderId = go.id
+	"
+	2.在情人节，收到两位以上好友的礼物用户，好友是谁？
+	"select gs.userId, u.name user, g.name goodsName,group_concat(uf.name)friends
+	from GoodsShipping gs
+	left join User u on gs.userId = u.id
+	left join UserFriend uf on gs.receiverId = uf.id
+	left join GoodsOrder go on gs.goodsOrderId = go.id
+	left join Goods g on g.id = go.goodsId
+	"
+	*/
 	@Test
 	public void params() {
 	}
